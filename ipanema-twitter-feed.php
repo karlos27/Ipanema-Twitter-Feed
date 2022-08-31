71463f2e92b78eb775e4f2cee80434f44bef092b
karlos27 <carlos.segarra@gmail.com>
2022-08-31 19:08:07 +0200

    First commit
    Main file with header

---
+++ ipanema-twitter-feed.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Ipanema Twitter Feed
+ * Plugin URI: https://github.com/karlos27/Ipanema-Film-Reviews.git
+ * Description: This plugin lets you add a twitter feed into your WordPress site.
+ * Author: segcgonz
+ * Author URI: https://github.com/karlos27
+ * Version: 1.0
+ * Requires at least: 6.0
+ * Tested up to: 6.0
+ * Requires PHP: 7.0
+ * Text Domain: ipanema-twitter-feed
+ * Domain Path: /languages
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
